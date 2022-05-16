001f79e7d0cfb1d047fa6e30a26bfb8f50bbb2e7
inesluparia <inesluparia@gmail.com>
2022-05-16 13:38:04 +0200

    Test2

--- src/app.py
+++ src/app.py
@@ -1,30 +1,30 @@
 from flask import Flask, request
-from keras.preprocessing.sequence import pad_sequences
-# from tensorflow.keras.models import load_model
-# import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+import pickle
 
 app = Flask(__name__)
 
 @app.route('/', methods=['post'])
 def predict():
     try:
-        # model = load_model('SpamHam.h5')
+        model = load_model('SpamHam.h5')
 
-        # with open('tokenizer.pickle', 'rb') as handle:
-        #     tokenizer = pickle.load(handle)
+        with open('tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
 
-        # message = request.json.get('message')
+        message = request.json.get('message')
 
-        # message = tokenizer.texts_to_sequences([message])
+        message = tokenizer.texts_to_sequences([message])
 
-        # message = pad_sequences(message, maxlen=100)
+        message = pad_sequences(message, maxlen=100)
 
-        # prediction = model.predict(message)[0][0]
+        prediction = model.predict(message)[0][0]
 
-        # isSpam = bool(prediction > 0.5)
+        isSpam = bool(prediction > 0.5)
         
-        # return { 'isSpam': isSpam }
-        return { 'isSpam': "hello" }
+        return { 'isSpam': isSpam }
+        # return { 'isSpam': "hello" }
 
     except Exception as e:
         print(str(e))
