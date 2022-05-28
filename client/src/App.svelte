3f879d9849ef0c3052b9d82f0c70b303bfe33ad1
inesluparia <inesluparia@gmail.com>
2022-05-28 16:20:25 +0200

    Bug fix

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -6,7 +6,7 @@
 
   async function handleClick () {
     isLoading = true
-    const response = await fetch('http://localhost:5000/api/predict', {
+    const response = await fetch('http://spamhampy.herokuapp.com/api/predict', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ message })
